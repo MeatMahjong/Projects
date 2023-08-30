7194b62bc30f910a79ab5bc31721ef457fd2732c
Emil Bratt <emilb@getacademy.no>
2023-08-30 14:10:17 +0200

    Rydder opp og fjerner script copy.js

--- Parprogrammering/sjakk/script.js
+++ Parprogrammering/sjakk/script.js
@@ -1,23 +1,35 @@
 //model
-let positions = getStartingPositions();
 const pieces = {
-    0: "black_rook_a",
-    1: "black_kight_a",
+    rook: "♜",
+    knight: "♞",
+    bishop: "♝",
+    queen: "♛",
+    king: "♚",
+    pawn: "♟"
 }
-// const pieces = "♜♞♝♛♚♝♞♜";
-const pieceRook = "♜";
+let positions = getStartingPositions();
+
 let board = document.getElementById("board");
+
 
 //view
 function updateView() {
     updateBoardView();
-    updatePieceView();
 }
 function updateBoardView() {
     for (let y = 0; y < 8; y++) {
         for(let x = 0; x < 8; x++) {
             let squareColor = (x+y) % 2 == 0 ? "lightSquare" : "darkSquare";
-            board.innerHTML += /*html*/`<div id="square_${y*8+x}" class="${squareColor}"></div>`;
+            let classes = /*html*/`${squareColor} `;
+            let tileContent = ""
+            let piece = positions[y][x];
+            if(piece != null) {
+                classes += piece.color;
+                tileContent = piece.piece
+            }
+            
+            
+            board.innerHTML += /*html*/`<div class="${classes}">${tileContent}</div>`;
         }
     }
 }
@@ -34,19 +46,22 @@ function updatePieceView(pieceId, ) {
 
 //helper functions
 function getStartingPositions() {
-    
-//     const pieces = "♜♞♝♛♚♝♞♜"
-//     let positions = [[]]
-//     for(let y = 0; y < 8; y++) {
-//         let row = []
-//         for(let x = 0; x < 8; x++) {
-//             let piece = y == 0 || y == 7 ? pieces.charAt(x) : y == 1 || y == 6 ? "♟" : ""
-//             let pieceColor = y >= 4 ? "whitePiece" : "blackPiece"
-//             row.push(`<div class=${pieceColor}>${piece}</div>`)
-//         }
-//         positions.push(row)
-//     }
-//     return positions
+    let positions = []
+
+    for(let y = 0; y < 8; y++) {
+        let formation = [pieces.rook, pieces.knight, pieces.bishop, pieces.queen, pieces.king, pieces.bishop, pieces.knight, pieces.rook]
+        let row = []
+        for(let x = 0; x < 8; x++) {
+            let piece = y == 0 || y == 7 ? formation[x] : y == 1 || y == 6 ? pieces.pawn : null
+            let pieceColor = y >= 4 ? "whitePiece" : "blackPiece"
+            if(piece != null) {
+                row.push({piece: piece, color: pieceColor, position: {x: x, y: y}})
+            }
+            else row.push(null)
+        }
+        positions.push(row)
+    }
+    return positions
 }
 
 
