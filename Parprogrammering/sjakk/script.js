025bbd7bafa1ba01ca84714e25fb997cb5851c5f
MeatMahjong <markus.m.eliassen@gmail.com>
2023-08-30 14:41:04 +0200

    Update script.js

--- Parprogrammering/sjakk/script.js
+++ Parprogrammering/sjakk/script.js
@@ -14,57 +14,50 @@ let board = document.getElementById("board");
 
 //view
 function updateView() {
-    updateBoardView();
-}
-function updateBoardView() {
+    board.innerHTML = ""
     for (let y = 0; y < 8; y++) {
         for(let x = 0; x < 8; x++) {
+            let onclick = ""
             let squareColor = (x+y) % 2 == 0 ? "lightSquare" : "darkSquare";
             let classes = /*html*/`${squareColor} `;
             let tileContent = ""
             let piece = positions[y][x];
             if(piece != null) {
+                onclick = movement()+`(${x}, ${y})`;
                 classes += piece.color;
                 tileContent = piece.piece
             }
             
-            
-            board.innerHTML += /*html*/`<div class="${classes}">${tileContent}</div>`;
+            board.innerHTML += /*html*/`<div class="${classes}" onclick="${onclick}">${tileContent}</div>`;
         }
     }
 }
-function updatePieceView(pieceId, ) {
-    for (let y = 0; y < 8; y++) {
-        for(let x = 0; x < 8; x++) {
-            let piece = document.getElementById("square_0")
-            piece.innerHTML = pieceRook
-        }
-    }
+
+function movement(piece) {
+    if(piece == "♟") return "movePawn"
 }
 
 //controller
+function movePawn(x,y) {
+    piece = positions[y][x]
+    positions[positions[y][x].color=="whitePiece" ? y-1 : y+1][x] = piece
+    positions[y][x] = null
+    updateView()
+}
 
 //helper functions
 function getStartingPositions() {
-    let positions = []
+    let formation = "♜♞♝♛♚♝♞♜"
+    let board = Array(8).fill(null).map(()=>Array(8).fill(null))
 
-    for(let y = 0; y < 8; y++) {
-        let formation = [pieces.rook, pieces.knight, pieces.bishop, pieces.queen, pieces.king, pieces.bishop, pieces.knight, pieces.rook]
-        let row = []
-        for(let x = 0; x < 8; x++) {
-            let piece = y == 0 || y == 7 ? formation[x] : y == 1 || y == 6 ? pieces.pawn : null
-            let pieceColor = y >= 4 ? "whitePiece" : "blackPiece"
-            if(piece != null) {
-                row.push({piece: piece, color: pieceColor, position: {x: x, y: y}})
-            }
-            else row.push(null)
-        }
-        positions.push(row)
-    }
-    return positions
+    for(let x = 0; x < 8; x++) board[0][x] = {piece: formation.charAt(x), color: "blackPiece"}
+    for(let x = 0; x < 8; x++) board[1][x] = {piece: "♟", color: "blackPiece"}
+
+    for(let x = 0; x < 8; x++) board[6][x] = {piece: "♟", color: "whitePiece"}
+    for(let x = 0; x < 8; x++) board[7][x] = {piece: formation.charAt(x), color: "whitePiece"}
+
+    return board
 }
-
-
 
 //main
 updateView()
