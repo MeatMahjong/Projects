15cdc5b14caef3e1bde488d79cf7d59541b9791e
MeatMahjong <markus.m.eliassen@gmail.com>
2023-08-30 15:28:54 +0200

    Slutt førte parprogrammerings økt

--- Parprogrammering/sjakk/script.js
+++ Parprogrammering/sjakk/script.js
@@ -1,16 +1,6 @@
 //model
-const pieces = {
-    rook: "♜",
-    knight: "♞",
-    bishop: "♝",
-    queen: "♛",
-    king: "♚",
-    pawn: "♟"
-}
 let positions = getStartingPositions();
-
 let board = document.getElementById("board");
-
 
 //view
 function updateView() {
@@ -23,7 +13,7 @@ function updateView() {
             let tileContent = ""
             let piece = positions[y][x];
             if(piece != null) {
-                onclick = movement()+`(${x}, ${y})`;
+                onclick = `${movement(x,y)}`+`(${x}, ${y})`;
                 classes += piece.color;
                 tileContent = piece.piece
             }
@@ -33,13 +23,68 @@ function updateView() {
     }
 }
 
-function movement(piece) {
-    if(piece == "♟") return "movePawn"
+function movement(x,y) {
+    let piece = positions[y][x];
+    if(piece == null) return;
+    if(piece.piece == "♟") return "movePawn";
 }
+
+function insideBoard(x,y) {
+    return x >= 0 || x >= 7 && y >= 0 || y >= 7;
+}
+
+function horseMoves(x,y) {
+    let legalMoves = [];
+    let color = positions[y][x].color;
+    
+    function checkMove(toX,toY) {
+        let target = positions[y][x];
+        if(insideBoard(x,y) && target == null || target.color != color)
+            legalMoves.push({y: toX, x: toY});
+    }
+
+    checkMove(x+1,y-2);
+    checkMove(x+1,y+2);
+    checkMove(x+2,y+1);
+    checkMove(x+2,y-1);
+    checkMove(x-1,y+2);
+    checkMove(x-1,y-2);
+    checkMove(x-2,y+1);
+    checkMove(x-2,y-1);
+
+    return legalMoves;
+}
+
+function diagonalMoves(x,y) {
+
+}
+
+function straightMoves(x,y) {
+
+}
+
+function queenMoves() {
+    let legalMoves = [];
+    diagonalMoves(legalMoves);
+    straightMoves(legalMoves);
+    return legalMoves;
+}
+
+function bishopMoves() {
+    let legalMoves = [];
+    diagonalMoves();
+    return legalMoves;
+}
+
+function rookMoves() {
+    let legalMoves = [];
+    straightMoves(legalMoves, x, y);
+}
+
 
 //controller
 function movePawn(x,y) {
-    piece = positions[y][x]
+    let piece = positions[y][x]
     positions[positions[y][x].color=="whitePiece" ? y-1 : y+1][x] = piece
     positions[y][x] = null
     updateView()
@@ -61,3 +106,23 @@ function getStartingPositions() {
 
 //main
 updateView()
+
+/*
+
+REGLER
+
+1. Sjekk om felt er ledig 
+2. Sjekk om din valgte brikke kan gå dit
+3. 
+
+HEST
+    1. Kan hoppe over
+
+BONDE
+    1. Kan gå frem hvis det er ledig
+    2. Kan gå frem 2 steg hvis den ikke har flyttet seg
+    3. Kan ta skrått fremover hvis det er en brikke der
+    4. Kan ta feltet bak en bonde som akkurat har gått 2 frem
+
+
+*/
